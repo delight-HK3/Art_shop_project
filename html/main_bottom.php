52e76ecb140e5a73eb6b788919aab80881e3b983
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-03 10:37:33 +0900

    Update main_bottom.php
    코드 정리 작업을 했습니다.

--- html/main_bottom.php
+++ html/main_bottom.php
@@ -13,86 +13,85 @@ html, body {
         height:1px;
       }
 </style>
-<head>
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
+
 <footer class="bg-light">
 <hr>
     <div class="container py-5">
-		<h4>You can find us at</h4>
-		<ul class="list-unstyled list-inline">
-			<li class="list-inline-item">
-				<a href="https://www.facebook.com/" class="sbtn btn-large mx-1" title="Facebook">
-				<h3><i class="fa fa-facebook-official" aria-hidden="true"></i><h3></a>
-			</li>
-			<li class="list-inline-item">
-				<a href="https://www.instagram.com/" class="sbtn btn-large mx-1" title="instagram">
-				<h3><i class="fa fa-instagram" aria-hidden="true"></i></h3></a>
-			</li>
-			<li class="list-inline-item">
-				<a href="https://twitter.com/?lang=ko" class="sbtn btn-large mx-1" title="Twitter">
-				<h3><i class="fa fa-twitter-square" aria-hidden="true"></i></h3></a>
-			</li>
-			<li class="list-inline-item">
-				<a href="https://www.youtube.com/" class="sbtn btn-large mx-1" title="Youtube">
-				<h3><i class="fa fa-youtube-play" aria-hidden="true"></i></h3></a>
-			</li>
-			<li class="list-inline-item">
-				<a href="https://kr.linkedin.com/" class="sbtn btn-large mx-1" title="Linkedin">
-				<h3><i class="fa fa-linkedin-square" aria-hidden="true"></i></h3></a>
-			</li>
-		</ul>
-         <div class="row py-3">
-             <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                 <h4 class="text-uppercase font-weight-bold mb-4 bg-primary">About</h4>
-                 <ul class="list-unstyled mb-0">
-                     <li class="mb-2"><a href="#" class="text-muted" >문의하기</a></li>
-                     <li class="mb-2"><a href="#" class="text-muted" >회사 소개</a></li>
-                     <li class="mb-2"><a href="#" class="text-muted" >소식</a></li>
-                 </ul>
-             </div>
-             <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                 <h4 class="text-uppercase font-weight-bold mb-4 bg-primary">Help</h4>
-                 <ul class="list-unstyled mb-0">
-                     <li class="mb-2"><a href="" class="text-muted" >주문 조회</a></li>
-                     <li class="mb-2"><a href="" class="text-muted" >배송 조회</a></li>
-                     <li class="mb-2"><a href="" class="text-muted" >주문 취소</a></li>
-                     <li class="mb-2"><a href="" class="text-muted" >환불 신청</a></li>
-                 </ul>
-             </div>
-             <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                 <h4 class="text-uppercase font-weight-bold mb-4 bg-primary">Policy</h4>
-                 <ul class="list-unstyled mb-0">
-                     <li class="mb-2"><a href="" class="text-muted" >반품 정책</a></li>
-                     <li class="mb-2"><a href="" class="text-muted" >이용 약관</a></li>
-                     <li class="mb-2"><a href="" class="text-muted" >개인정보 약관</a></li>
-                 </ul>
-             </div>
-             <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                 <h4 class="text-uppercase font-weight-bold mb-4 bg-primary">Company</h4>
-                 <ul class="list-unstyled mb-0">
-                     <li class="mb-2"><a href="member_login.php" class="text-muted" >Login</a></li>
-                     <li class="mb-2"><a href="member_agree.php" class="text-muted">회원가입</a></li>
-                 </ul>
-             </div>
-             <div class="col-lg-4 col-md-6 mb-lg-0">
-                 <h4 class="text-uppercase font-weight-bold mb-4 bg-primary" style="font-weight: bold;">회사주소 및 기타 정보</h4>
-                 <p class="text-muted mb-4">상호 : 행복화방 | 대표 : 이다빈<br>사업자 등록번호 : 341-20-10932<br>개인정보담당자 : 이다빈<br>전화 : 02-131-4591 Fax : 02-654-9321<br>주소 : 서울특별시 노원구 월계2동 초안산로 12<br>copylight (c) www.happyArt.co.kr All Right reserved </p>
-                 
-             </div>
-         </div>
-		<div id="copyrights" class="bg-light">
-            <div id="bottom-container" class="container clearfix">
-                <div class="row justify-content-between align-items-center">       
-                    <div class="col-md-6 d-md-flex flex-md-column align-items-md-end mt-4 mt-md-0">
-                         <div class="copyrights-menu copyright-links clearfix">
-							<img src="//img.echosting.cafe24.com/skin/admin_ko_KR/common/admin_fair_trade.gif">
-                            <img src="//img.echosting.cafe24.com/skin/admin_ko_KR/common/admin_cafe24.gif">
-                            <img src="//img.echosting.cafe24.com/skin/admin_ko_KR/common/admin_ssl.gif">
-                         </div>
-                    </div>
-                </div>
-            </div>
+	<h4>You can find us at</h4>
+	<ul class="list-unstyled list-inline">
+		<li class="list-inline-item">
+			<a href="https://www.facebook.com/" class="sbtn btn-large mx-1" title="Facebook">
+			<h3><i class="fa fa-facebook-official" aria-hidden="true"></i><h3></a>
+		</li>
+		<li class="list-inline-item">
+			<a href="https://www.instagram.com/" class="sbtn btn-large mx-1" title="instagram">
+			<h3><i class="fa fa-instagram" aria-hidden="true"></i></h3></a>
+		</li>
+		<li class="list-inline-item">
+			<a href="https://twitter.com/?lang=ko" class="sbtn btn-large mx-1" title="Twitter">
+			<h3><i class="fa fa-twitter-square" aria-hidden="true"></i></h3></a>
+		</li>
+		<li class="list-inline-item">
+			<a href="https://www.youtube.com/" class="sbtn btn-large mx-1" title="Youtube">
+			<h3><i class="fa fa-youtube-play" aria-hidden="true"></i></h3></a>
+		</li>
+		<li class="list-inline-item">
+			<a href="https://kr.linkedin.com/" class="sbtn btn-large mx-1" title="Linkedin">
+			<h3><i class="fa fa-linkedin-square" aria-hidden="true"></i></h3></a>
+		</li>
+	</ul>
+        <div class="row py-3">
+        	<div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                	<h4 class="text-uppercase font-weight-bold mb-4 bg-primary">About</h4>
+                 	<ul class="list-unstyled mb-0">
+				<li class="mb-2"><a href="#" class="text-muted" >문의하기</a></li>
+				<li class="mb-2"><a href="#" class="text-muted" >회사 소개</a></li>
+				<li class="mb-2"><a href="#" class="text-muted" >소식</a></li>
+                 	</ul>
+             	</div>
+             	<div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                	<h4 class="text-uppercase font-weight-bold mb-4 bg-primary">Help</h4>
+                 	<ul class="list-unstyled mb-0">
+                     		<li class="mb-2"><a href="" class="text-muted" >주문 조회</a></li>
+                     		<li class="mb-2"><a href="" class="text-muted" >배송 조회</a></li>
+                     		<li class="mb-2"><a href="" class="text-muted" >주문 취소</a></li>
+                     		<li class="mb-2"><a href="" class="text-muted" >환불 신청</a></li>
+                	</ul>
+             	</div>
+             	<div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                	<h4 class="text-uppercase font-weight-bold mb-4 bg-primary">Policy</h4>
+                	<ul class="list-unstyled mb-0">
+                     		<li class="mb-2"><a href="" class="text-muted" >반품 정책</a></li>
+                     		<li class="mb-2"><a href="" class="text-muted" >이용 약관</a></li>
+                     		<li class="mb-2"><a href="" class="text-muted" >개인정보 약관</a></li>
+                	</ul>
+             	</div>
+             	<div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                	<h4 class="text-uppercase font-weight-bold mb-4 bg-primary">Company</h4>
+                 	<ul class="list-unstyled mb-0">
+                     		<li class="mb-2"><a href="member_login.php" class="text-muted" >Login</a></li>
+                     		<li class="mb-2"><a href="member_agree.php" class="text-muted">회원가입</a></li>
+                 	</ul>
+             	</div>
+             	<div class="col-lg-4 col-md-6 mb-lg-0">
+                	<h4 class="text-uppercase font-weight-bold mb-4 bg-primary" style="font-weight: bold;">회사주소 및 기타 정보</h4>
+                	<p class="text-muted mb-4">상호 : 행복화방 | 대표 : 이다빈<br>사업자 등록번호 : 341-20-10932<br>개인정보담당자 : 이다빈<br>전화 : 02-131-4591 Fax : 02-654-9321<br>주소 : 서울특별시 노원구 월계2동 초안산로 12<br>copylight (c) www.happyArt.co.kr All Right reserved </p>
+             	</div>
+        </div>
+	<div id="copyrights" class="bg-light">
+        	<div id="bottom-container" class="container clearfix">
+                	<div class="row justify-content-between align-items-center">       
+                    		<div class="col-md-6 d-md-flex flex-md-column align-items-md-end mt-4 mt-md-0">
+                         		<div class="copyrights-menu copyright-links clearfix">
+						<img src="//img.echosting.cafe24.com/skin/admin_ko_KR/common/admin_fair_trade.gif">
+                            			<img src="//img.echosting.cafe24.com/skin/admin_ko_KR/common/admin_cafe24.gif">
+                            			<img src="//img.echosting.cafe24.com/skin/admin_ko_KR/common/admin_ssl.gif">
+                         		</div>
+                    		</div>
+                	</div>
+            	</div>
 		<p></p>
-    </div>
+    	</div>
  </footer>
