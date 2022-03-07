ec784a5538de93482e425172fbb5013d613539a8
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-07 10:22:10 +0900

    Update opt_edit.php
    코드 정리 작업을 했습니다.

--- html/admin/opt_edit.php
+++ html/admin/opt_edit.php
@@ -1,9 +1,4 @@
-<!-------------------------------------------------------------------------------------------->	
-<!-- 프로그램 : 쇼핑몰 따라하기 실습지시서 (실습용 HTML)                                    -->
-<!--                                                                                        -->
-<!-- 만 든 이 : 윤형태 (2008.2 - 2017.12)                                                    -->
-<!-------------------------------------------------------------------------------------------->	
-<?
+<?php
 	include "../common.php";
 	$no=$_REQUEST[no];
 	$query="select * from opt where no11 = $no;";
@@ -14,55 +9,44 @@
 	$row=mysqli_fetch_array($result); //1레코드 읽기
 ?>
 <html>
-<head>
-<title>쇼핑몰 관리자 홈페이지</title>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<link rel="stylesheet" href="include/font.css">
-<script language="JavaScript" src="include/common.js"></script>
-</head>
-
-<body style="margin:0">
-
-<center>
-
-<br>
-<script> document.write(menu());</script>
-<br>
-<br>
-
-<form name="form1" method="post" action="opt_update.php">
-
-<input type="hidden" name="no" value="<?=$no?>">
-
-<table width="500" border="1" cellpadding="2" style="border-collapse:collapse">
-	<tr> 
-		<td width="100" height="20" bgcolor="#CCCCCC" align="center">
-			<font color="#142712">옵션번호</font>
-		</td>
-		<td width="400" height="20"  bgcolor="#F2F2F2"><?=$row[no11]?></td>
-	</tr>
-	<tr> 
-		<td width="100" height="20" bgcolor="#CCCCCC" align="center">
-			<font color="#142712">옵션명</font>
-		</td>
-		<td width="400" height="20"  bgcolor="#F2F2F2">
-			<input type="text" name="name" value="<?=$row[name11]?>" size="20" maxlength="20">
-		</td>
-	</tr>
-</table>
-<br>
-<table width="500" border="0" cellspacing="0" cellpadding="7">
-	<tr> 
-		<td align="center">
-			<input type="submit" value="수 정 하 기"> &nbsp;&nbsp
-			<input type="button" value="이 전 화 면" onClick="javascript:history.back();">
-		</td>
-	</tr>
-</table>
-
-</form>
-
-</center>
-
-</body>
+	<head>
+		<title>쇼핑몰 관리자 홈페이지</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" href="include/font.css">
+		<script language="JavaScript" src="include/common.js"></script>
+		<script> document.write(menu());</script>
+	</head>
+		<body style="margin:0">
+			<center>
+			<br><br><br>
+			<form name="form1" method="post" action="opt_update.php">
+				<input type="hidden" name="no" value="<?php echo $no?>">
+				<table width="500" border="1" cellpadding="2" style="border-collapse:collapse">
+					<tr> 
+						<td width="100" height="20" bgcolor="#CCCCCC" align="center">
+							<font color="#142712">옵션번호</font>
+						</td>
+						<td width="400" height="20"  bgcolor="#F2F2F2"><?php echo $row[no11]?></td>
+					</tr>
+					<tr> 
+						<td width="100" height="20" bgcolor="#CCCCCC" align="center">
+							<font color="#142712">옵션명</font>
+						</td>
+						<td width="400" height="20"  bgcolor="#F2F2F2">
+							<input type="text" name="name" value="<?php echo $row[name11]?>" size="20" maxlength="20">
+						</td>
+					</tr>
+				</table>
+				<br>
+				<table width="500" border="0" cellspacing="0" cellpadding="7">
+					<tr> 
+						<td align="center">
+							<input type="submit" value="수 정 하 기"> &nbsp;&nbsp
+							<input type="button" value="이 전 화 면" onClick="javascript:history.back();">
+						</td>
+					</tr>
+				</table>
+			</form>
+		</center>
+	</body>
 </html>
